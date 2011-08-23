44740a085b29ff50994c9a4dc33d60da1076c8b2
Oliver Gondža <324706@mail.muni.cz>
2011-08-23 20:38:46 +0200

    Usage info (-h) provided

--- phpregtest.sh
+++ phpregtest.sh
@@ -7,7 +7,28 @@ php_version="?.*.*"
 # Default phpfarm inst dir
 phpfarm_dst="/opt/phpfarm"
 
+# Arguments passed to the runner
 runner_args=()
+
+function printUsage () {
+
+  program=`basename $0`;
+
+  echo "Usage: ${program} [OPTION]...
+Run phpunit tests using php interpreters installed via phpfarm.
+
+${program} locates phpunit and phpfarm automatically.
+
+-f DIR          locates php interpreters in DIR directory.
+-r FILE         use FILE test runner.
+-v PATTERN      run test using php version(s) constrained by pattern.
+-h              display this help and exit
+
+PATTERN is string used by ls command. It can contain wildcards and character
+classes.
+  
+All the other arguments are passed to the test runner.";
+}
 
 while [ $# -gt 0 ]
 do
@@ -15,6 +36,7 @@ do
     (-f) phpfarm_dst=$2; shift;;
     (-r) test_runner=$2; shift;;
     (-v) php_version=$2; shift;;
+    (-h) printUsage; exit 0;;
     (*) runner_args+=($1);;
   esac
   shift
